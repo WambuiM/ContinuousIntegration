6eb7e0381a4067b187c12e430aa5a11b7454879c
Margaret Maina <mmaina@innova.co.ke>
2022-02-16 20:18:01 +0300

    Added .circleci folder

--- test_Calculator.py
+++ test_Calculator.py
@@ -7,12 +7,16 @@ class TestCalculator(unittest.TestCase):
     def test_addition(self):
         self.assertEqual(Calculator.addition(5, 5), 10)
         self.assertEqual(Calculator.addition(-3, 3), 0)
-        self.assertEqual(Calculator.addition(-5, -5), 10)
+        self.assertEqual(Calculator.addition(-5, -5), -10)
 
     def test_subtraction(self):
         self.assertEqual(Calculator.subtraction(15, 5), 10)
         self.assertEqual(Calculator.subtraction(-1, 2), -3)
         self.assertEqual(Calculator.subtraction(-1, -1), 0)
         self.assertEqual(Calculator.subtraction(-1, -3), 2)
-        self.assertEqual(Calculator.subtraction(6, -9), 3)
-        self.assertEqual(Calculator.subtraction(-6, 3), -3)
+        self.assertEqual(Calculator.subtraction(6, -9), 15)
+        self.assertEqual(Calculator.subtraction(-6, 3), -9)
+
+
+if __name__ == '__main__':
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
